3ee93582671444dac42c6f520ead94670d5f8d09
wuao <18225560719@163.com>
2020-05-09 18:41:50 +0800

    162.find-peak-element

---
+++ src/FindPeakElement.java
@@ -0,0 +1,23 @@
+/**
+ * FindPeakElement
+ * 2020-05-09 18:29
+ *
+ * @author wuao
+ */
+public class FindPeakElement {
+
+    public int findPeakElement(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return -1;
+        int left = 0, right = nums.length - 1, mid;
+        while (left < right) {
+            mid = (right + left) >> 1;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
