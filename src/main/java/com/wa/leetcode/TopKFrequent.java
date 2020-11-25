c71485106a42e382b90940c97c41f95dd3dc95de
wuao <18225560719@163.com>
2020-11-25 19:39:30 +0800

    347. 前 K 个高频元素

---
+++ src/main/java/com/wa/leetcode/TopKFrequent.java
@@ -0,0 +1,42 @@
+package com.wa.leetcode;
+
+import javafx.util.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * TopKFrequent
+ * 2020-11-25 19:07
+ *
+ * @author wuao
+ */
+public class TopKFrequent {
+    public static void main(String[] args) {
+        int[] res = topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2);
+        for (int num : res) {
+            System.out.print(num + ",");
+        }
+    }
+
+
+    public static int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Pair<Integer, Integer>> priorityQueue = new PriorityQueue<>(k, (o1, o2) -> o2.getValue() - o1.getValue());
+        for (Map.Entry entry : map.entrySet()) {
+            priorityQueue.offer(new Pair(entry.getKey(), entry.getValue()));
+        }
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = priorityQueue.poll().getKey();
+        }
+
+        return res;
+    }
+}
