bd261aa5b9c71f483d4b2aeef15784687e65bde4
wuao <18225560719@163.com>
2023-07-02 15:50:58 +0800

    2. 两数相加

---
+++ src/main/java/com/wa/leetcode/AddTwoNumbers.java
@@ -0,0 +1,68 @@
+package com.wa.leetcode;
+
+import com.wa.model.ListNode;
+
+/**
+ * AddTwoNumbers
+ * https://leetcode.cn/problems/add-two-numbers/
+ * 2. 两数相加
+ * 2023/7/2 3:40 PM
+ *
+ * @author wuao
+ */
+public class AddTwoNumbers {
+
+    /*
+    给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+    请你将两个数相加，并以相同形式返回一个表示和的链表。
+    你可以假设除了数字 0 之外，这两个数都不会以 0开头。
+
+    示例 1：
+        输入：l1 = [2,4,3], l2 = [5,6,4]
+        输出：[7,0,8]
+        解释：342 + 465 = 807.
+    示例 2：
+        输入：l1 = [0], l2 = [0]
+        输出：[0]
+    示例 3：
+        输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+        输出：[8,9,9,9,0,0,0,1]
+
+    提示：
+        每个链表中的节点数在范围 [1, 100] 内
+        0 <= Node.val <= 9
+        题目数据保证列表表示的数字不含前导零
+    */
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode cur = head;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry > 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+            }
+            if (sum < 10) {
+                carry = 0;
+            } else {
+                carry = 1;
+                sum -= 10;
+            }
+            ListNode next = new ListNode(sum);
+            cur.next = next;
+            cur = next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return head.next;
+    }
+}
